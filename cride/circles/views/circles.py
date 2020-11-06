7839fd28bbd169265ec26c1f1df0d78382060721
cris-linux <cadasmeq02@gmail.com>
2020-11-06 14:29:34 -0300

    Clase 15 : se implementa CircleViewSet con su ModelSerializer

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -3,38 +3,16 @@
 # Django REST Framework
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
 # Models
 from cride.circles.models import Circle
 
 # Serializers
-from cride.circles.serializers import (
-    CircleSerializer,
-    CreateCircleSerializer
-)
+from cride.circles.serializers import CircleModelSerializer
 
-class CirclesViewSet(viewsets.ViewSet):
+class CirclesViewSet(viewsets.ModelViewSet):
     """Circles ViewSet"""
 
-    def list(self, request):
-        queryset = Circle.objects.all()
-        serializer = CircleSerializer(queryset, many=True)
-        return Response(serializer.data)
-    
+    queryset = Circle.objects.all()
+    serializer_class = CircleModelSerializer
 
-@api_view(['GET'])
-def list_circles(request):
-    """List circles."""
-    circles = Circle.objects.filter(is_public=True)
-    serializer = CircleSerializer(circles, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def create_circle(request):
-    """Create circle."""
-    serializer = CreateCircleSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    circle = serializer.save()
-    return Response(CircleSerializer(circle).data)
